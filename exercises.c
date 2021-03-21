c0bb44ba279c731ef4622df64ccaafe965fbfaf8
Replit user <replituser@example.com>
2021-03-21 03:19:03 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -129,7 +129,7 @@ void sumaV(Vector * a, Vector * b, Vector * c) {
   int j=sizeof(*a);
   int n=(j/k);
   int i;
-  printf("%d",j);
+  printf("%d",k);
   for(i=0;i<n;i++){
     c->datos[i]=a->datos[i]+b->datos[i];
   }
